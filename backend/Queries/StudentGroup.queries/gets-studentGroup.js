2aa8f11183d11d535f72e53810e14a71becb32fc
Daniel Salazar <dannysalazarm07@gmail.com>
2018-05-01 13:38:55 -0500

    one ´part of get and post of estudiante_grupo table

---
+++ backend/Queries/StudentGroup.queries/gets-studentGroup.js
@@ -0,0 +1,28 @@
+var connection = require('../../connection');
+
+function MethodsDB() {
+
+    this.selectQuery = function (queryParams,response) {
+        var id=queryParams.id?queryParams.id:null
+        var id_group=queryParams.id_group?queryParams.id_group:null
+        var id_student=queryParams.id_student?queryParams.id_student:null
+        var id_course=queryParams.id_course?queryParams.id_course:null
+        connection.obtain(function (er, cn) {
+            cn.query('select * from estudiante_grupo where (id_estudiante_grupo = '+id+' or '+id+' is NULL ) AND ' + 
+            '(id_grupo = '+id_group+' or '+id_group+' is NULL ) AND ' +
+            '(id_estudiante = '+id_student+' or '+id_student+' is NULL ) AND ' +
+            '(id_asignatura = '+id_course+' or '+id_course+' is NULL )',
+                function (error, result) {
+                cn.release();
+                if (error) {
+                    response.send({ state: 'Error search by query params estudiante_grupo' })
+                } else {
+                    response.send(result);
+                }
+            })
+        })
+    }
+
+}
+
+module.exports = new MethodsDB();
