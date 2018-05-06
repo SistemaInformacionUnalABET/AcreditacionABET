4c066516ee5d0c6fe1bb970847a1bc1599b301b3
Miguel Torres <miatorresch@unal.edu.co>
2018-05-06 09:15:45 -0500

    changes in backend

--- backend/Queries/Evaluations.queries/gets-evaluations.js
+++ backend/Queries/Evaluations.queries/gets-evaluations.js
@@ -25,7 +25,8 @@ function MethodsDB() {
         var id_asig_ind=queryParams.id_asig_ind?queryParams.id_asig_ind:null
 
         connection.obtain(function (er, cn) {
-            cn.query('select * from evaluaciones where (id_evaluacion = '+id+' or '+id+' is NULL ) AND ' + 
+            cn.query('select * from evaluaciones where '+
+            '(id_evaluacion = '+id+' or '+id+' is NULL ) AND ' + 
             '(tipo_evaluacion = '+type+' or '+type+' is NULL ) AND ' +
             ' (evidencia_url = '+url+' or '+url+' is NULL ) AND (id_asignatura_indicador = '+id_asig_ind+' or '+id_asig_ind+' is NULL )',
                 function (error, result) {
