99d2f3f9595930ca665a450612f07434e8c6fc7e
Daniel Salazar <dannysalazarm07@gmail.com>
2018-06-14 07:37:33 -0500

    three graphics

--- frontendABET/src/app/statistics/graphics/byCourse/graphics-by-course-average/graphics-by-course-average.component.ts
+++ frontendABET/src/app/statistics/graphics/byCourse/graphics-by-course-average/graphics-by-course-average.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
-//import { ChartModule, ChartComponent } from 'angular2-highcharts';
+import { Component, OnInit, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
-//import { chart } from 'highcharts';
-import { print } from 'util';
+//Original hihgcharts
+// import { chart } from 'highcharts';
+// import * as Highcharts from 'highcharts';
+//Original hihgcharts
+
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 
-import { Chart, Highcharts, HIGHCHARTS_MODULES, ChartModule, ɵa, ɵb, MapChart, StockChart } from 'angular-highcharts';
+import { Chart, HIGHCHARTS_MODULES, ChartModule, ɵa, ɵb, MapChart, StockChart } from 'angular-highcharts';
 
 import { ViewCompleteGrade } from './../../../statistics/entities/viewCompleteGrade';
 import { Course } from '../../../../statistics/statistics/entities/course';
 import { UploadService } from './../../../../uploadData/uploadData/uploadData.service';
 import { GraphicsService } from '../../graphics.service';
-import { ChartDirective } from 'angular-highcharts/chart.directive';
-import { ChartObject, ChartOptions } from 'highcharts';
+//import { ChartDirective } from 'angular-highcharts/chart.directive';
+///import { ChartObject, ChartOptions } from 'highcharts';
 
-//import * as Highcharts from 'highcharts';
 declare var require: any;
 declare var jQuery: any;
 declare var $: any;
@@ -29,8 +30,6 @@ declare var $: any;
 })
 export class GraphicsByCourseAverageComponent implements OnInit {
 
-  // @ViewChild('chartTarget') chartTarget: ElementRef;
-  // chart: Highcharts.ChartObject;
 
   completeGradesList: ViewCompleteGrade[];
   courseList: Course[];
@@ -44,9 +43,7 @@ export class GraphicsByCourseAverageComponent implements OnInit {
   indicatorsAVG;
   indicatorsGradesCount;
 
-  //highchartO : Highcharts;
-
-
+  //chart: Highcharts.ChartObject;
   chart: Object;
   constructor(private graphicsService: GraphicsService, private uploadService: UploadService) {
     this.completeGradesList = [];
@@ -57,6 +54,53 @@ export class GraphicsByCourseAverageComponent implements OnInit {
     this.graphicsService.changeMessage(this.completeGradesList);
 
   }
+
+  //@ViewChild('chartTarget') chartTarget: ElementRef;
+
+  // ngAfterViewInit() {
+
+  //   const options: Highcharts.Options = {
+
+  //     chart: {
+  //       type: 'column'
+  //     },
+  //     plotOptions: {
+  //       series: {
+  //         // general options for all series
+  //       },
+  //       column: {
+  //         colorByPoint: true,
+  //         showInLegend: false
+  //       }
+  //     },
+
+  //     title: {
+  //       text: 'Chart.update'
+  //     },
+
+  //     subtitle: {
+  //       text: 'Plain'
+  //     },
+
+  //     xAxis: {
+  //       categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  //     },
+
+  //     yAxis: {
+  //       title: {
+  //         text: 'Fruit eaten'
+  //       }
+  //     },
+
+  //     series: [{
+  //      // type: 'column',
+  //       data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+  //     }]
+
+  //   };
+  //   this.chart = chart(this.chartTarget.nativeElement, options);
+
+  // }
 
   ngOnInit() {
 
@@ -84,72 +128,128 @@ export class GraphicsByCourseAverageComponent implements OnInit {
     console.log("-->ENTRANDOOOOOOOOOOOOOOOOOOOOO");
 
     //Prueba otra grafica
-    this.chart = new Chart({
-      chart: {
-        type: 'column'
-      },
-      title: {
-        text: 'Desempeño de los estudiante por asignatura'
-      },
-      xAxis: {
-        categories: ['2017-2', '2018-1']
-      },
-      yAxis: {
-        min: 0,
-        title: {
-          text: 'Total de estudiantes evaluados'
-        },
-        stackLabels: {
-          enabled: true,
-          style: {
-            fontWeight: 'bold',
-            color: 'gray'
-          }
-        }
-      },
-      legend: {
-        align: 'right',
-        x: -30,
-        verticalAlign: 'top',
-        y: 25,
-        floating: true,
-        backgroundColor: 'white',
-        borderColor: '#CCC',
-        borderWidth: 1,
-        shadow: false
-      },
-      tooltip: {
-        headerFormat: '<b>{point.x}</b><br/>',
-        pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
-      },
-      plotOptions: {
-        column: {
-          stacking: 'normal',
-          dataLabels: {
-            enabled: true,
-            color: 'white'
-          }
-        }
-      },
-      series: [{
-        name: 'Ejemplar',
-        data: [10, 20,]
-      }, {
-        name: 'Satisfactorio',
-        data: [20,20]
-      }, {
-        name: 'Desarrollado',
-        data: [50,40]
-      },{
-        name: 'Insatisfactorio',
-        data: [20,20]
-      }]
-    });
+    // this.chart = new Chart({
+    //   chart: {
+    //     type: 'column'
+    //   },
+    //   title: {
+    //     text: 'Desempeño de los estudiante por asignatura'
+    //   },
+    //   xAxis: {
+    //     categories: arrayX
+    //   },
+    //   yAxis: {
+    //     min: 0,
+    //     title: {
+    //       text: 'Total de estudiantes evaluados'
+    //     },
+    //     stackLabels: {
+    //       enabled: true,
+    //       style: {
+    //         fontWeight: 'bold',
+    //         color: 'gray'
+    //       }
+    //     }
+    //   },
+    //   legend: {
+    //     align: 'right',
+    //     x: -30,
+    //     verticalAlign: 'top',
+    //     y: 25,
+    //     floating: true,
+    //     backgroundColor: 'white',
+    //     borderColor: '#CCC',
+    //     borderWidth: 1,
+    //     shadow: false
+    //   },
+    //   tooltip: {
+    //     headerFormat: '<b>{point.x}</b><br/>',
+    //     pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+    //   },
+    //   plotOptions: {
+    //     column: {
+    //       stacking: 'normal',
+    //       dataLabels: {
+    //         enabled: true,
+    //         color: 'white'
+    //       }
+    //     }
+    //   },
+    //   series: [{
+    //     name: 'Ejemplar',
+    //     data: [10, 20,]
+    //   }, {
+    //     name: 'Satisfactorio',
+    //     data: [20,20]
+    //   }, {
+    //     name: 'Desarrollado',
+    //     data: [50,40]
+    //   },{
+    //     name: 'Insatisfactorio',
+    //     data: [20,20]
+    //   }]
+    // });
     //Prueba otra grafica
 
+    //Prueba 3
+      this.chart = new Chart({
+
+        chart: {
+            type: 'column'
+        },
+
+        title: {
+            text: 'Highcharts responsive chart'
+        },
+
+        subtitle: {
+            text: 'Resize the frame or click buttons to change appearance'
+        },
+
+        legend: {
+            align: 'right',
+            verticalAlign: 'middle',
+            layout: 'vertical'
+        },
+
+        xAxis: {
+            categories: ['Apples', 'Oranges', 'Bananas'],
+            labels: {
+                x: -10
+            }
+        },
+
+        yAxis: {
+            allowDecimals: false,
+            title: {
+                text: 'Amount'
+            }
+        },
+
+        series: [{
+            name: 'Christmas Eve',
+            data: [1, 4, 3]
+        }, {
+            name: 'Christmas Day before dinner',
+            data: [6, 4, 2]
+        }, {
+            name: 'Christmas Day after dinner',
+            data: [8, 4, 3]
+        }],
+
+    });
+    //prueba 3
 
     // this.chart = new Chart({
-
+    //   chart: {
+    //     type: 'column'
+    //   },
+    //   plotOptions: {
+    //     column: {
+    //       colorByPoint: true,
+    //       showInLegend: false
+    //     }
+    //   },
     //   title: {
     //     text: 'Chart.update',
     //   },
@@ -165,18 +265,9 @@ export class GraphicsByCourseAverageComponent implements OnInit {
 
     //   series: [{
     //     type: 'column',
-    //     colorByPoint: true,
-    //     data: arrayY,
-    //     //showInLegend: false
-
+    //     data: arrayY
     //   }],
 
-    //   plotOptions: {
-    //     bar: {
-    //       colorByPoint: true,
-    //       showInLegend: false
-    //     }
-    // }
     // });
   }
 
