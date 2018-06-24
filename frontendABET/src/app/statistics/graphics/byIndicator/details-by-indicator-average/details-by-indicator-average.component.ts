adccfba7d6191de52aef3fb0f391bde60a1bc98b
Daniel Salazar <dannysalazarm07@gmail.com>
2018-06-24 12:40:01 -0500

    html by indicator more button download excel

--- frontendABET/src/app/statistics/graphics/byIndicator/details-by-indicator-average/details-by-indicator-average.component.ts
+++ frontendABET/src/app/statistics/graphics/byIndicator/details-by-indicator-average/details-by-indicator-average.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ViewCompleteGrade } from './../../../statistics/entities/viewCompleteGrade';
 import { GraphicsService } from '../../graphics.service';
 
+import * as XLSX from 'xlsx';
+
 @Component({
   selector: 'app-details-by-indicator-average',
   templateUrl: './details-by-indicator-average.component.html',
@@ -9,11 +11,18 @@ import { GraphicsService } from '../../graphics.service';
 })
 export class DetailsByIndicatorAverageComponent implements OnInit {
 
-  @Input() course: number;
+
+
+  data: any = [[1, 2, 3, 4, 4], [3, 4, 3, 1, 2]];
+  dataForExport: any;
+  // wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+  fileName: string = 'SheetJS.xlsx';
 
   flagGrades = false;
 
   completeGradesList: ViewCompleteGrade[];
+
+  @Input() course: number;
 
   constructor(private graphicsService: GraphicsService) {
     this.completeGradesList = [];
@@ -21,16 +30,9 @@ export class DetailsByIndicatorAverageComponent implements OnInit {
   ngOnInit() {
 
 
-    alert(this.course + "ALERT");
+    alert(this.course + "ALERT by indicator");
 
-    // this.graphicsService.currentMessage.subscribe(listComplete => {
-    //   this.completeGradesList = listComplete
-    //   if (this.completeGradesList.length > 0) {
-    //     this.flagGrades = true;
-    //   } else {
-    //     this.flagGrades = false;
-    //   }
-    // })
+
 
     if (this.course != null || this.course != undefined) {
 
@@ -43,6 +45,37 @@ export class DetailsByIndicatorAverageComponent implements OnInit {
         }
       })
     }
-
   }
+
+  export(): void {
+    /* generate worksheet */
+
+    this.iteratecompleteGradesList();
+    console.log("dataForExport --> = " + this.dataForExport);
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.dataForExport);
+
+    console.log("complete list = ", );
+
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
+  }
+
+  iteratecompleteGradesList() {
+
+    this.dataForExport.push(["id_asignatura", "numero_grupo", "id_indicador", "identificador_indicador", "tipo_evaluacion", "tipo_actividad",
+      "documento", "calificacion", "descripcion_calificacion", "periodo", "fecha_creacion", "fecha_modificacion", "observacion", "evidencia_url"])
+
+    for (let element of this.completeGradesList) {
+      this.dataForExport.push([element.id_asignatura, element.numero_grupo, element.id_indicador, element.identificador_indicador,
+      element.tipo_actividad, element.tipo_actividad, element.documento, element.calificacion, element.descripcion_calificacion,
+      element.periodo, element.fecha_creacion, element.fecha_modificacion, element.observacion, element.evidencia_url
+      ])
+    }
+  }
+
 }
